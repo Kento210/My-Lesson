21f195889ab1e3bf0b3051fcaf878265b01b05ca
Kento210 <kento.inoue@so-tech.co.jp>
2024-03-19 13:22:13 +0900

    update: Todoの状態変化をさせるためのコンポーネントを追加

--- Redux/lesson-redux-app/src/components/TodoList.js
+++ Redux/lesson-redux-app/src/components/TodoList.js
@@ -1,15 +1,23 @@
 // 型定義機能を提供する PropTypes を import します。
 import PropTypes from 'prop-types'
 
+import Todo from './Todo'
+
 // TodoList Component には、TodoList Container で定義した
 // todos と toggleTodo が渡されています。
 const TodoList = ({ todos, toggleTodo }) => (
-  <ul>
-    {todos.map(todo =>
-      <li>{todo.text}</li>
-    )}
-  </ul>
-)
+    <ul>
+      {todos.map(todo =>
+        // ここで Todo コンポーネントを描画します。
+        // onClick には、toggleTodo（引数は todo の id）を渡します。
+        <Todo
+          key={todo.id}
+          {...todo}
+          onClick={() => toggleTodo(todo.id)}
+        />
+      )}
+    </ul>
+  )
 
 // 下記では、TodoList Component に渡される引数（propsといいます）について
 // 「データ型」・「必須か否か」を定義しています。
