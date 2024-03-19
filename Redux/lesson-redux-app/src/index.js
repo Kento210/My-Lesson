485c5488361c30918be9407e80bba704fbd20506
Kento210 <kento.inoue@so-tech.co.jp>
2024-03-19 12:30:18 +0900

    update: Storeの追加

--- Redux/lesson-redux-app/src/index.js
+++ Redux/lesson-redux-app/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import todosReducer from './reducers';
+
+const store = createStore(
+  todosReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
